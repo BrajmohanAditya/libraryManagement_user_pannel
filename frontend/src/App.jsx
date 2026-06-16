68a98dcb98bde2d54df0b570528879f5ffe48b6f
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-06-16 15:26:22 +0530

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <>
       {/* <div><MainRoutes/></div> */}
          {/* <DashboardDataViewer /> */}
-          {/* <LibraryDetailViewer /> */}
-          <ReviewDataViewer />
+          <LibraryDetailViewer />
+          {/* <ReviewDataViewer /> */}
     </>
   )
 }
